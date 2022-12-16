136f78160ebfe2f0c24a272fff966debc06a09ff
+ <+>
2022-12-16 10:39:59 +0300

    add README.md

--- README.md
+++ README.md
@@ -8,12 +8,18 @@ that gives overview,rating,title and the coverpage.
 
 ## set up
 
-navigate in the project directory()
+navigate in the project directory 
 
-clone [here] ()
+using terminal command(`cd to Desktop`)
+
+navigate to a folder called phase-1-project using command(`cd phase-1-project`)
 
 open the visual studio code by typing in the terminal
 `code .`
+
+navigate to `movie-search app` 
+clone [here] (git@github.com:57-83/movie-search-app.git)
+
 ## installation
 ### requirements
  * vs code
--- index.js
+++ index.js
@@ -21,40 +21,40 @@ function showMovies(movies) {
     movieEl.className = "movie-info";
     movieEl.classList.add("movie");
     movieEl.innerHTML = `
-//         <img src="${IMGPATH}${poster_path}"alt="${title}">
-//         <div class="movie-info">
-//             <h3>${title}</h3>
-//             <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-//         </div>
-//         <div class="overview">
-//             <h3>overview</h3>
-//             ${overview}
-//         </div>
-//     `;
-//     main.appendChild(movieEl);
-//   });
-// }
-// function getClassByRate(vote) {
-//   if (vote >= 8) {
-//     return "green";
-//   } else if (vote >= 5) {
-//     return "orange";
-//   } else {
-//     return "red";
-//   }
-// }
-// function searchMovies(url) {
-//   // Fetch the movie data from the API using the provided URL
-//   fetch(url)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       // When the data is received, show the movies using the showMovies function
-//       showMovies(data.results);
-//     });
-// }
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   const searchTerm = search.value;
-//   searchMovies(SEARCHAPI + searchTerm);
-//   search.value = "";
-// });
+        <img src="${IMGPATH}${poster_path}"alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h3>overview</h3>
+            ${overview}
+        </div>
+    `;
+    main.appendChild(movieEl);
+  });
+}
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+function searchMovies(url) {
+  // Fetch the movie data from the API using the provided URL
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // When the data is received, show the movies using the showMovies function
+      showMovies(data.results);
+    });
+}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+  searchMovies(SEARCHAPI + searchTerm);
+  search.value = "";
+});
